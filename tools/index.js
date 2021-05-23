3179c396a44f58b7a57f84b14fd611c0c921069c
Carlos Huerta Garcia <huerta2502@gmail.com>
2021-05-23 00:19:20 -0500

    Tabs fully working

--- tools/index.js
+++ tools/index.js
@@ -7,6 +7,7 @@ function load() {
     document.getElementById("queueTab").addEventListener("click", function () { showTabs(2); });
     document.getElementById("listTab").addEventListener("click", function () { showTabs(3); });
     document.getElementById("dark").addEventListener("change", darkMode, false);
+    showTabs(0);
 }
 
 function darkMode() {
@@ -26,24 +27,56 @@ function showTabs(i) {
             document.getElementById("stack").style.display = "block";
             document.getElementById("queue").style.display = "none";
             document.getElementById("list").style.display = "none";
+            document.getElementById("aboutTab").style.borderBottom = "0ex";
+            document.getElementById("aboutTab").style.backgroundColor = "";
+            document.getElementById("stackTab").style.borderBottom = "0.3ex solid #A9F5F2";
+            document.getElementById("stackTab").style.backgroundColor = "rgba(16, 16, 16, 0.2)";
+            document.getElementById("queueTab").style.borderBottom = "0ex";
+            document.getElementById("queueTab").style.backgroundColor = "";
+            document.getElementById("listTab").style.borderBottom = "0ex";
+            document.getElementById("listTab").style.backgroundColor = "";
             break;
         case 2:
             document.getElementById("about").style.display = "none";
             document.getElementById("stack").style.display = "none";
             document.getElementById("queue").style.display = "block";
             document.getElementById("list").style.display = "none";
+            document.getElementById("aboutTab").style.borderBottom = "0ex";
+            document.getElementById("aboutTab").style.backgroundColor = "";
+            document.getElementById("stackTab").style.borderBottom = "0ex";
+            document.getElementById("stackTab").style.backgroundColor = "";
+            document.getElementById("queueTab").style.borderBottom = "0.3ex solid #A9F5F2";
+            document.getElementById("queueTab").style.backgroundColor = "";
+            document.getElementById("listTab").style.borderBottom = "0ex";
+            document.getElementById("listTab").style.backgroundColor = "";
             break;
         case 3:
             document.getElementById("about").style.display = "none";
             document.getElementById("stack").style.display = "none";
             document.getElementById("queue").style.display = "none";
             document.getElementById("list").style.display = "block";
+            document.getElementById("aboutTab").style.borderBottom = "0ex";
+            document.getElementById("aboutTab").style.backgroundColor = "";
+            document.getElementById("stackTab").style.borderBottom = "0ex";
+            document.getElementById("stackTab").style.backgroundColor = "";
+            document.getElementById("queueTab").style.borderBottom = "0ex";
+            document.getElementById("queueTab").style.backgroundColor = "";
+            document.getElementById("listTab").style.borderBottom = "0.3ex solid #A9F5F2";
+            document.getElementById("listTab").style.backgroundColor = "rgba(16, 16, 16, 0.2)";
             break;
         default:
             document.getElementById("about").style.display = "block";
             document.getElementById("stack").style.display = "none";
             document.getElementById("queue").style.display = "none";
             document.getElementById("list").style.display = "none";
+            document.getElementById("aboutTab").style.borderBottom = "0.3ex solid #A9F5F2";
+            document.getElementById("aboutTab").style.backgroundColor = "rgba(16, 16, 16, 0.2)";
+            document.getElementById("stackTab").style.borderBottom = "0ex";
+            document.getElementById("stackTab").style.backgroundColor = "";
+            document.getElementById("queueTab").style.borderBottom = "0ex";
+            document.getElementById("queueTab").style.backgroundColor = "";
+            document.getElementById("listTab").style.borderBottom = "0ex";
+            document.getElementById("listTab").style.backgroundColor = "";
             break;
     }
 }
